93414e503b3d4b7d63aa0fb716b1ed900fc50497
GuidoSantiagoReta <guidosantiagoreta@gmail.com>
2024-04-21 11:46:10 -0300

    img inicio

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 
 
-// Closes responsive menu when a scroll trigger link is clicked
-
-$('.nav-link').on('click', function(){
+// Cierra el menú responsivo cuando se hace clic en un enlace de activación de desplazamiento
+$('.nav-link').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
 
 
 // Función para remover la clase activa en los enlaces 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var navLinks = document.querySelectorAll('.nav-link');
 
-    navLinks.forEach(function(link) {
-        link.addEventListener('click', function() {
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
             // Remover la clase 'active' de todos los enlaces
-            navLinks.forEach(function(link) {
+            navLinks.forEach(function (link) {
                 link.classList.remove('active');
             });
 
@@ -24,9 +23,10 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-window.onload = function() {
-    document.querySelector('a[data-toggle="collapse"][href="#inicio"]').addEventListener('click', function(e) {
-      e.preventDefault();
-      window.scrollTo({top: 0, behavior: 'smooth'});
+// función para volver al inicio
+window.onload = function () {
+    document.querySelector('a[data-toggle="collapse"][href="#inicio"]').addEventListener('click', function (e) {
+        e.preventDefault();
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     });
-  };
+};
